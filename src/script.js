4d31c50878f3406bc8c535c8c1db2a5e53fd3651
Maria Piankova <mariapankova46@gmail.com>
2022-12-19 19:08:44 +0200

    fn for navbar

---
+++ src/script.js
@@ -0,0 +1,9 @@
+let content = document.querySelector(".row__header");
+function toggleContent(){
+  console.log('dlkfjgdflsgjdfgkds');
+  if(content.style.display===""){
+    content.style.display = "none";
+  } else {
+    content.style.display = "";
+  }
+}
